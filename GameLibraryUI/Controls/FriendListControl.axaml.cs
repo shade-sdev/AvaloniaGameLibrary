a8ed1527d9c0dd96ebfb9bbb00e7987f30d0177a
Shade <Shade@Shade.ga>
2025-03-20 19:03:33 +0400

    Modularize Friend List Control

---
+++ GameLibraryUI/Controls/FriendListControl.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GameLibraryUI.Controls;
+
+public partial class FriendListControl : UserControl
+{
+    public FriendListControl()
+    {
+        InitializeComponent();
+    }
+}
